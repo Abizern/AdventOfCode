c9c42867a30d3f39da4ab1f1cae0d1363dbdde94
Abizer Nasir <abizern@abizern.org>
2021-12-06 07:33:39 +0000

    Solve 2021 day6

--- Advent21/Sources/Solution6.swift
+++ Advent21/Sources/Solution6.swift
@@ -2,15 +2,47 @@ import AdventUtilities
 import Foundation
 
 public enum Solution6: Solution {
-    public static var title = ""
+    public static var title = "--- Day 6: Lanternfish ---"
 
     public static func part1(_ input: String) -> String {
-        "Not Solved Yet"
+        let inputDictionary = generateDictionary(input)
+
+        return "\(countPopulation(of: inputDictionary, over: 80))"
     }
 
     public static func part2(_ input: String) -> String {
-        "Not Solved Yet"
+        let inputDictionary = generateDictionary(input)
+
+        return "\(countPopulation(of: inputDictionary, over: 256))"
     }
+}
 
+extension Solution6 {
+    static func generateDictionary(_ input: String) -> [Int: Int] {
+        Dictionary(input
+                    .safeString
+                    .components(separatedBy: ",")
+                    .compactMap(Int.init)
+                    .map { ($0, 1) },
+                   uniquingKeysWith: +)
+    }
+    static func countPopulation(of input: [Int: Int], over: Int) -> Int {
+        var memo = input
 
+        (0..<over).forEach { _ in
+            var temp = [Int: Int]()
+            memo.forEach { (key, value) in
+                if key == 0 {
+                    temp[6, default: 0] += value
+                    temp[8, default: 0] += value
+                } else {
+                    temp[key - 1, default: 0] += value
+                }
+            }
+
+            memo = temp
+        }
+
+        return memo.values.reduce(0, +)
+    }
 }
---
+++ Advent21/Tests/Solution06Tests.swift
@@ -0,0 +1,17 @@
+import XCTest
+import Advent21
+
+final class Solution6Tests: XCTestCase {
+    func testPart1() {
+        XCTAssertEqual(Solution6.part1(testInput), "5934")
+    }
+
+    func testPart2() {
+        XCTAssertEqual(Solution6.part2(testInput), "26984457539")
+    }
+}
+
+private let testInput =
+"""
+3,4,3,1,2
+""".trimmingCharacters(in: .whitespacesAndNewlines)
