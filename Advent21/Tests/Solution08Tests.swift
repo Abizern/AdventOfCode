bd226b996008105925974cfc3730444914bee50f
Abizer Nasir <abizern@abizern.org>
2021-12-07 07:01:45 +0000

    Prep for 2021 day 8

---
+++ Advent21/Tests/Solution08Tests.swift
@@ -0,0 +1,17 @@
+import XCTest
+import Advent21
+
+final class Solution8Tests: XCTestCase {
+    func testPart1() {
+        XCTAssertEqual(Solution8.part1(testInput), "")
+    }
+
+    func testPart2() {
+        XCTAssertEqual(Solution8.part2(testInput), "")
+    }
+}
+
+private let testInput =
+"""
+
+""".trimmingCharacters(in: .whitespacesAndNewlines)
