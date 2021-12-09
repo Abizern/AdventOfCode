884fefd90f37ad2dea245134234a38c00d1e0986
Abizer Nasir <abizern@abizern.org>
2021-12-09 08:24:46 +0000

    Solve 2021 day9 part 1
    Quick solution to get to part 2

--- Advent21/Sources/Solution9.swift
+++ Advent21/Sources/Solution9.swift
@@ -1,16 +1,50 @@
 import AdventUtilities
-import Foundation
+import Swift
 
 public enum Solution9: Solution {
-    public static var title = ""
+    public static var title = "--- Day 9: Smoke Basin ---"
 
     public static func part1(_ input: String) -> String {
-        "Not Solved Yet"
+        let grid = parseInput(input)
+        let rows = grid.count
+        let cols = grid[0].count
+        let rowRange = (0 ..< rows)
+        let colRange = (0 ..< cols)
+
+        var heightValue = 0
+        rowRange.forEach { row in
+            colRange.forEach { col in
+                var isLowest = true
+                let rowChange = [-1, 0, 1, 0]
+                let colChange = [0, 1, 0, -1]
+                (0 ..< 4).forEach { changeIdx in
+                    let r = row + rowChange[changeIdx]
+                    let c = col + colChange[changeIdx]
+
+                    if rowRange.contains(r) && colRange.contains(c) && grid[r][c] <= grid[row][col] {
+                        isLowest = false
+                    }
+                }
+                if isLowest {
+                    heightValue += grid[row][col] + 1
+                }
+            }
+        }
+
+        return String(describing: heightValue)
     }
 
     public static func part2(_ input: String) -> String {
         "Not Solved Yet"
     }
+}
 
-
+extension Solution9 {
+    struct Point: Hashable {
+        let x: Int
+        let y: Int
+    }
+    static func parseInput(_ input: String) -> [[Int]] {
+        input.trimmingCharacters(in: .whitespacesAndNewlines).lines.compactMap { $0.compactMap(\.wholeNumberValue) }
+    }
 }
---
+++ Advent21/Tests/Solution09Tests.swift
@@ -0,0 +1,22 @@
+import XCTest
+import Advent21
+
+final class Solution9Tests: XCTestCase {
+    func testPart1() {
+        XCTAssertEqual(Solution9.part1(testInput), "15")
+    }
+
+    func testPart2() {
+        XCTAssertEqual(Solution9.part2(testInput), "")
+    }
+}
+
+private let testInput =
+"""
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+
+"""
