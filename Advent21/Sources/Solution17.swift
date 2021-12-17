84db85df5233c17fa233127b9b31144cf281cc53
Abizer Nasir <abizern@abizern.org>
2021-12-17 09:25:02 +0000

    Solve 2021 day17
    Originally worked out in a playground. This is just a move and tidy

--- Advent21/Sources/Solution17.swift
+++ Advent21/Sources/Solution17.swift
@@ -1,18 +1,70 @@
 import AdventUtilities
 import Foundation
+import Parsing
 
 public enum Solution17: Solution {
-    public static var title = ""
+    public static var title = "--- Day 17: Trick Shot ---"
 
     public static func part1(_ input: String) -> String {
-        return "Not Solved Yet"
+        let (_, maxHeight) = evaluate(parse(input))
+
+        return "\(maxHeight)"
     }
 
     public static func part2(_ input: String) -> String {
-        return "Not Solved Yet"
+        let (count, _) = evaluate(parse(input))
+
+        return "\(count)"
     }
 }
 
 extension Solution17 {
-    
+    static func parse(_ input: String) -> (Int, Int, Int, Int) {
+
+        guard let values = Skip("target area: x=").take(Int.parser()).skip("..").take(Int.parser()).skip(", y=").take(Int.parser()).skip("..").take(Int.parser()).parse(input) else {
+            fatalError("Can't parse input \(input)")
+        }
+
+        return values
+    }
+
+    static func evaluate(_ targetValues: (Int, Int, Int, Int)) -> (Int, Int) {
+        let (x1, x2, y1, y2) = targetValues
+        let minX = min(x1, x2)
+        let maxX = max(x1, x2)
+        let minY = min(y1, y2)
+        let maxY = max(y1, y2)
+
+        var maxHeightReached = Int.min
+        var count = 0
+        (0 ... maxX).forEach { initialX in
+            (minY ... max(maxY, abs(minY))).forEach { initialY in
+                var maxHeight = Int.min
+                var found = false
+                var currentX = 0
+                var currentY = 0
+                var vX = initialX
+                var vY = initialY
+
+                while !found && currentX < maxX && currentY > minY {
+                    currentX += vX
+                    currentY += vY
+                    maxHeight = max(currentY, maxHeight)
+                    if vX > 0 { vX -= 1 }
+                    vY -= 1
+                    if (minX ... maxX).contains(currentX) && (minY ... maxY).contains(currentY) {
+                        found = true
+                        continue
+                    }
+                }
+
+                if found {
+                    count += 1
+                    maxHeightReached = max(maxHeightReached, maxHeight)
+                }
+            }
+        }
+
+        return (count, maxHeightReached)
+    }
 }
--- Advent21/Tests/Solution17Tests.swift
+++ Advent21/Tests/Solution17Tests.swift
@@ -3,16 +3,16 @@ import Advent21
 
 final class Solutionn17Tests: XCTestCase {
     func testPart1() {
-        XCTAssertEqual(Solution17.part1(testInput), "")
+        XCTAssertEqual(Solution17.part1(testInput), "45")
     }
 
     func testPart2() {
-        XCTAssertEqual(Solution17.part2(testInput), "")
+        XCTAssertEqual(Solution17.part2(testInput), "112")
     }
 }
 
 private let testInput =
 """
-
+target area: x=20..30, y=-10..-5
 
 """
