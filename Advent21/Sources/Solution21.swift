659304b5d666a01103ef2041688d756748671488
Abizer Nasir <abizern@abizern.org>
2021-12-20 16:09:17 +0000

    Prep for 2021 day 21

--- Advent21/Sources/Solution21.swift
+++ Advent21/Sources/Solution21.swift
@@ -5,12 +5,14 @@ public enum Solution21: Solution {
     public static var title = ""
 
     public static func part1(_ input: String) -> String {
-        "Not Solved Yet"
+        return "Not Solved Yet"
     }
 
     public static func part2(_ input: String) -> String {
-        "Not Solved Yet"
+        return "Not Solved Yet"
     }
+}
 
-
+extension Solution21 {
+    
 }
