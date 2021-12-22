5ba186e9c892fa8c1870f1a3f653a1fa30a00bcb
Abizer Nasir <abizern@abizern.org>
2021-12-22 16:51:08 +0000

    Solve 2021 day22 part 2
    Takes about a minute to run

--- Advent21/Sources/Solution22.swift
+++ Advent21/Sources/Solution22.swift
@@ -29,7 +29,35 @@ public enum Solution22: Solution {
     }
 
     public static func part2(_ input: String) -> String {
-        "Not Solved Yet"
+        let rawValues = parseRawValues(input)
+        let (xValToIndex, xIndexToLength) = mapping(Set(rawValues.map(\.1).flatMap { [$0.0, $0.1 + 1] }))
+        let (yValToIndex, yIndexToLength) = mapping(Set(rawValues.map(\.2).flatMap { [$0.0, $0.1 + 1] }))
+        let (zValToIndex, zIndexToLength) = mapping(Set(rawValues.map(\.3).flatMap { [$0.0, $0.1 + 1] }))
+        var reactor: Set<Cube> = .init()
+
+        rawValues.forEach { (state, xs, ys, zs) in
+            (xValToIndex[xs.0]! ..< xValToIndex[xs.1 + 1]!).forEach { x in
+                (yValToIndex[ys.0]! ..< yValToIndex[ys.1 + 1]!).forEach { y in
+                    (zValToIndex[zs.0]! ..< zValToIndex[zs.1 + 1]!).forEach { z in
+                        let cube = Cube(x: x, y: y, z: z)
+                        switch state {
+                        case .on:
+                            reactor.insert(cube)
+                        default:
+                            reactor.remove(cube)
+                        }
+                    }
+                }
+            }
+        }
+
+        let result = reactor.map { cube in
+            xIndexToLength[cube.x]! * yIndexToLength[cube.y]! * zIndexToLength[cube.z]!
+        }.reduce(0, +)
+
+
+
+        return "\(result)"
     }
 }
 
@@ -79,5 +107,53 @@ private extension Solution22 {
             .map(makeInstruction)
 
         return Many(instructionParser, separator: "\n").parse(&string) ?? []
+    }
+}
+
+
+// Part 2
+private extension Solution22 {
+    typealias RawValue = (State, (Int, Int), (Int, Int), (Int, Int))
+
+    struct Cube: Hashable {
+        let x: Int
+        let y: Int
+        let z: Int
+    }
+
+    static func parseRawValues(_ input: String) -> [RawValue] {
+        var string = input[...]
+        let rangeParser = Int.parser().skip("..").take(Int.parser())
+        let instructionParser = PrefixUpTo(" ")
+            .skip(" x=")
+            .take(rangeParser)
+            .skip(",y=")
+            .take(rangeParser)
+            .skip(",z=")
+            .take(rangeParser)
+            .map { parsed in
+                (State(parsed.0), parsed.1, parsed.2, parsed.3)
+            }
+
+        guard let rawValues = Many(instructionParser, separator: "\n").parse(&string) else {
+            fatalError("Can't parse input as expected")
+        }
+
+        return rawValues
+    }
+
+    static func mapping(_ values: Set<Int>) -> ([Int: Int], [Int: Int]) {
+        let sortedValues = values.sorted()
+        let numberOfValues = sortedValues.count
+        var valueToIndex = [Int: Int]()
+        var indexToLength = [Int: Int]()
+        for (i, value) in sortedValues.enumerated() {
+            valueToIndex[value] = i
+            if i + 1 < numberOfValues {
+                indexToLength[i] = sortedValues[i + 1] - value
+            }
+        }
+
+        return (valueToIndex, indexToLength)
     }
 }
