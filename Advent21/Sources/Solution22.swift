42f21f5c8e8e2b1f9d26414f9a08034144a20114
Abizer Nasir <abizern@abizern.org>
2021-12-22 07:32:27 +0000

    Solve 2021 day22 part 1
    Somewhat brute force, just to get to part 2 quickly

--- Advent21/Sources/Solution22.swift
+++ Advent21/Sources/Solution22.swift
@@ -1,16 +1,69 @@
 import AdventUtilities
 import Foundation
+import Parsing
 
 public enum Solution22: Solution {
-    public static var title = ""
+    public static var title = "--- Day 22: Reactor Reboot ---"
 
     public static func part1(_ input: String) -> String {
-        "Not Solved Yet"
+        let instructions = parse(input)
+        let cubeRange = (-50 ... 50)
+
+        var count = 0
+        cubeRange.forEach { x in
+            cubeRange.forEach { y in
+                cubeRange.forEach { z in
+                    var cube = (x, y, z, 0)
+                    for instruction in instructions {
+                        instruction(&cube)
+                    }
+                    if cube.3 == 1 {
+                        count += 1
+                    }
+                }
+            }
+        }
+
+        return "\(count)"
     }
 
     public static func part2(_ input: String) -> String {
         "Not Solved Yet"
     }
+}
 
+private extension Solution22 {
+    static func makeInstruction(_ values: (Substring, (Int, Int), (Int, Int), (Int, Int))) -> (inout (Int, Int, Int, Int)) -> Void {
+        let instruction: String = String(values.0)
+        let xRange = values.1
+        let yRange = values.2
+        let zRange = values.3
 
+        return { cube in
+            let (x, y, z, _) = cube
+            if (xRange.0 ... xRange.1).contains(x) && (yRange.0 ... yRange.1).contains(y) && (zRange.0 ... zRange.1).contains(z) {
+                if instruction == "on" {
+                    cube = (x, y, z, 1)
+                } else {
+                    cube = (x, y, z, 0)
+                }
+            }
+        }
+    }
+
+    // on x=10..12,y=10..12,z=10..12
+    static func parse(_ input: String) -> [(inout (Int, Int, Int, Int)) -> Void] {
+        var string = input[...]
+        let rangeParser = Int.parser().skip("..").take(Int.parser())
+        let instructionParser = PrefixUpTo(" ")
+            .skip(" x=")
+            .take(rangeParser)
+            .skip(",y=")
+            .take(rangeParser)
+            .skip(",z=")
+            .take(rangeParser)
+            .map(makeInstruction)
+
+        return Many(instructionParser, separator: "\n").parse(&string) ?? []
+    }
 }
