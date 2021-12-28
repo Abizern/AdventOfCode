81545a0f30916e541f0ad887ce3e2da18312f337
Abizer Nasir <abizern@abizern.org>
2021-12-28 19:34:45 +0000

    Add extra inputs

--- Advent21/Sources/Solution24.swift
+++ Advent21/Sources/Solution24.swift
@@ -2,15 +2,35 @@ import AdventUtilities
 import Foundation
 
 public enum Solution24: Solution {
-    public static var title = ""
+    public static var title = "--- Day 24: Arithmetic Logic Unit ---"
 
     public static func part1(_ input: String) -> String {
-        "Not Solved Yet"
+        return "Not Solved Yet"
     }
 
     public static func part2(_ input: String) -> String {
-        "Not Solved Yet"
+        return "Not Solved Yet"
     }
+}
 
+private extension Solution24 {
+    private static func parse(_ input: String) {
 
+    }
+}
+
+private extension Solution24 {
+    enum Op {
+        enum ParamType {
+            case key(String)
+            case value(Int)
+        }
+
+        case inp(String)
+        case add(String, ParamType)
+        case mul(String, ParamType)
+        case div(String, ParamType)
+        case mod(String, ParamType)
+        case eql(String, ParamType)
+    }
 }
